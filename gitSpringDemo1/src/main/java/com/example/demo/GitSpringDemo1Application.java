e03572b9dca2e56c2f2b4b0508ab01164aa77924
Mitsuru Okubo <okubomi@gmail.com>
2021-01-25 15:21:59 +0900

    spring bootサンプルの初回コミット

---
+++ gitSpringDemo1/src/main/java/com/example/demo/GitSpringDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringDemo1Application.class, args);
+	}
+
+}
